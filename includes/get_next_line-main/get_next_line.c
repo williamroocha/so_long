a99afc131c6242117454fe53051cdb860d7b1c4b
William Rocha <william@pop-os.localdomain>
2023-10-17 20:38:20 +0100

    started the project

---
+++ includes/get_next_line-main/get_next_line.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 13:05:42 by wiferrei          #+#    #+#             */
+/*   Updated: 2023/05/10 07:51:13 by wiferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char		*ln;
+	static char	buffer[BUFFER_SIZE + 1];
+	int			i;
+
+	i = 1;
+	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
+	{
+		i = -1;
+		while (buffer[++i])
+			buffer[i] = 0;
+		return (NULL);
+	}
+	ln = NULL;
+	while (i > 0)
+	{
+		if (!buffer[0])
+			i = read(fd, buffer, BUFFER_SIZE);
+		if (i > 0)
+			ln = ft_strjoin(ln, buffer);
+		if (ft_find_nl(buffer) > 0)
+			break ;
+	}
+	return (ln);
+}
---
+++ includes/get_next_line-main/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 13:06:37 by wiferrei          #+#    #+#             */
+/*   Updated: 2023/05/10 07:36:31 by wiferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_find_nl(char *str);
+int		ft_strlen(char *str);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif

3071a07904695d6a8f9b5d87cbbc1913ca147f1b
William Rocha <william@pop-os.localdomain>
2023-12-04 08:49:06 +0000

    Solved one of the many reachable leaks

--- src/util/check_map_extension.c
+++ src/util/check_map_extension.c
@@ -6,46 +6,19 @@
 /*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 16:06:26 by wiferrei          #+#    #+#             */
-/*   Updated: 2023/12/03 12:06:04 by wiferrei         ###   ########.fr       */
+/*   Updated: 2023/12/04 08:47:26 by wiferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/so_long.h"
 
-char	*make_path(char *map_path)
-{
-	int		length;
-	char	*path;
-	int		i;
-
-	if (map_path == NULL || map_path[0] == '\0')
-		return (NULL);
-	length = 0;
-	while (map_path[length + 1] != '\0')
-		length++;
-	path = (char *)malloc(length + 1);
-	if (path == NULL)
-		return (NULL);
-	i = 1;
-	while (i <= length)
-	{
-		path[i - 1] = map_path[i];
-		i++;
-	}
-	path[length] = '\0';
-	return (path);
-}
-
-void	check_map_extension(char *map_path)
+void	check_map_extension(char *path)
 {
 	char	*extension;
-	char	*path;
 
-	path = make_path(map_path);
 	extension = ft_strchr(path, '.');
 	if (extension == NULL)
 		error_handler("Error\nMap file has no extension.\n", NULL);
 	if (ft_strcmp(extension, BER) != 0)
 		error_handler("Error\nMap file has wrong extension.\n", NULL);
-	free(path);
 }
--- src/util/directions.c
+++ src/util/directions.c
@@ -6,7 +6,7 @@
 /*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 15:38:30 by wiferrei          #+#    #+#             */
-/*   Updated: 2023/12/02 11:57:57 by wiferrei         ###   ########.fr       */
+/*   Updated: 2023/12/04 08:35:08 by wiferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void	move_right(t_game *game, t_coord *coord, t_coord *last_coord,
 		int is_player)
 {
-	if (coord->x < (game->map->width * BLOCK_PIXEL) && !is_exit(game, coord)
-		&& !is_right_wall(game, coord))
+	if (coord->x < (game->map->width * BLOCK_PIXEL) && !is_right_wall(game,
+			coord))
 	{
 		last_coord->x = coord->x;
 		coord->x += BLOCK_PIXEL;
@@ -28,7 +28,7 @@ void	move_right(t_game *game, t_coord *coord, t_coord *last_coord,
 void	move_left(t_game *game, t_coord *coord, t_coord *last_coord,
 		int is_player)
 {
-	if (coord->x > 0 && !is_exit(game, coord) && !is_left_wall(game, coord))
+	if (coord->x > 0 && !is_left_wall(game, coord))
 	{
 		last_coord->x = coord->x;
 		coord->x -= BLOCK_PIXEL;
@@ -40,7 +40,7 @@ void	move_left(t_game *game, t_coord *coord, t_coord *last_coord,
 void	move_up(t_game *game, t_coord *coord, t_coord *last_coord,
 		int is_player)
 {
-	if (coord->y > 0 && !is_exit(game, coord) && !is_top_wall(game, coord))
+	if (coord->y > 0 && !is_top_wall(game, coord))
 	{
 		last_coord->y = coord->y;
 		coord->y -= BLOCK_PIXEL;
@@ -52,8 +52,8 @@ void	move_up(t_game *game, t_coord *coord, t_coord *last_coord,
 void	move_down(t_game *game, t_coord *coord, t_coord *last_coord,
 		int is_player)
 {
-	if (coord->y < (game->map->height * BLOCK_PIXEL) && !is_exit(game, coord)
-		&& !is_down_wall(game, coord))
+	if (coord->y < (game->map->height * BLOCK_PIXEL) && !is_down_wall(game,
+			coord))
 	{
 		last_coord->y = coord->y;
 		coord->y += BLOCK_PIXEL;
